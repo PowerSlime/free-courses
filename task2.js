d3404c106be80ab9876322afeec65ece02c1c5fc
PowerSlime <powerslime@ya.ru>
2018-06-03 20:01:28 +0300

    Add support for negative numbers

--- task2.js
+++ task2.js
@@ -1,10 +1,15 @@
 const fib = n => {
+	let isNegative = n < 0;
+	let isEven = n % 2 == 0;
+
+	n = Math.abs(n);  // If it is negative then will make positive
+
 	let [a, b] = [1, 0];
 	while (n-- > 0) {
 		[a, b] = [b + a, a];
 	}
 
-	return b;
+	return (isNegative && isEven) ? b * -1 : b;
 };
 
 const number = parseInt(process.argv[2]);  // Number from CLI
